483ceed4c7ddc2661ec2ffc69354935efdef24bf
otreblan <otreblain@gmail.com>
2022-10-07 10:51:25 -0500

    Use hittable in object node

--- src/cpr_tree/cpr_tree_active_object.h
+++ src/cpr_tree/cpr_tree_active_object.h
@@ -4,9 +4,10 @@
 #define CONE_TREE_CPR_TREE_ACTIVE_OBJECT_H
 
 #include "cpr_tree_node.hpp"
+#include "../hittable.hpp"
 
 class cpr_tree_active_object : public cpr_tree_node {
-    void* data;
+    hittable* data;
     ~cpr_tree_active_object() noexcept override = default;
 };
 
